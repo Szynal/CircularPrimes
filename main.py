f6483b6915e3dbe9a5e0f04f9e5c84bd86de0d7a
Paweł Szynal <pawel@szynal.info>
2021-07-08 19:01:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-counter = 0
-
-
-def sieve_of_eratosthenes(number):
+def sieve_of_eratosthenes( number ):
     counter = 0
     prime = [True for i in range(number + 1)]
     p = 2
@@ -20,25 +17,30 @@ def sieve_of_eratosthenes(number):
                 re_roll_number = int(n)
                 if prime[re_roll_number]:
                     local_counter = local_counter + 1
-            if (local_counter == len(p_combinations)):
+            if local_counter == len(p_combinations):
                 counter = counter + 1
     return counter
 
 
-def get_iter_combinations(number):
+def get_iter_combinations( number ):
     str_nr = list(str(number))
     numbers = []
     for i in range(len(str_nr)):
         str_nr.append(str_nr.pop(str_nr.index(str_nr[0])))
         a = ''.join(str_nr)
-        while a[:1] == '0':  # Wywalanie  zer  ['1', '10', '100', '1000', '10000', '100000', '1000000'] dla np miliona
+        while a[:1] == '0':
             a = a[1:]
-        if a in numbers:  # dla  np 111
+        if a in numbers:
             continue
         numbers.append(a)
     return numbers
 
 
+def circular_checker( n ):
+    return sieve_of_eratosthenes(n)
+
+
 if __name__ == '__main__':
-    n = 100
-    print(sieve_of_eratosthenes(n))
+    #    print(circular_checker(100))
+    print(circular_checker(10 ** 6))
+#    assert (circular_checker(1000000) == 55)
