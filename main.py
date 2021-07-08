8dd811e28015d4bf63dd6448056073025d42b97e
Paweł Szynal <pawel@szynal.info>
2021-07-08 18:38:28 +0200

    Working demo version

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
 counter = 0
 
-def sieve_of_eratosthenes( number ):
+
+def sieve_of_eratosthenes(number):
     counter = 0
     prime = [True for i in range(number + 1)]
     p = 2
@@ -14,19 +14,24 @@ def sieve_of_eratosthenes( number ):
     prime[1] = False
     for p in range(number + 1):
         if prime[p]:
-            get_iter_combinations(p)
-            print(p)
+            local_counter = 0
+            p_combinations = get_iter_combinations(p)
+            for n in p_combinations:
+                re_roll_number = int(n)
+                if prime[re_roll_number]:
+                    local_counter = local_counter + 1
+            if (local_counter == len(p_combinations)):
+                counter = counter + 1
+    return counter
 
 
-def get_iter_combinations( number ):
+def get_iter_combinations(number):
     str_nr = list(str(number))
     numbers = []
     for i in range(len(str_nr)):
         str_nr.append(str_nr.pop(str_nr.index(str_nr[0])))
         a = ''.join(str_nr)
-        # print(str_nr)
-        print(a)
-        while a[:1] == '0': # Wywalanie  zer  ['1', '10', '100', '1000', '10000', '100000', '1000000'] dla np miliona
+        while a[:1] == '0':  # Wywalanie  zer  ['1', '10', '100', '1000', '10000', '100000', '1000000'] dla np miliona
             a = a[1:]
         if a in numbers:  # dla  np 111
             continue
@@ -35,6 +40,5 @@ def get_iter_combinations( number ):
 
 
 if __name__ == '__main__':
-    n = 1000000
-    # sieve_of_eratosthenes(n)
-    get_iter_combinations(n)
+    n = 100
+    print(sieve_of_eratosthenes(n))
